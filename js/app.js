3de1c15480f8a5ebf03db8b9bfa0b60063a44fa7
rahulyadav1986 <rahullooksforward@gmail.com>
2021-08-05 19:09:59 +0530

    All Updated

--- js/app.js
+++ js/app.js
@@ -34,3 +34,43 @@ function mobile_cross(){
     mobileSearchPanel.classList.remove('active');
 }
 
+function register(){
+    var main_login_area= document.getElementById('main_login_area');
+    var log_text= document.getElementById('log_text');
+    main_login_area.classList.add('active');
+    log_text.classList.add('active');
+    log_text.classList.remove('for_pass');
+    main_login_area.classList.remove('forgot');
+    social_del_back.classList.remove('social_button_off');
+}
+function login(){
+    var main_login_area= document.getElementById('main_login_area');
+    var log_text= document.getElementById('log_text');
+    main_login_area.classList.remove('active');
+    log_text.classList.remove('active')
+}
+
+function forgot(){
+    var main_login_area= document.getElementById('main_login_area');
+    var social_del_back= document.getElementById('social_del_back');
+    log_text.classList.add('for_pass');
+    social_del_back.classList.add('social_button_off');
+    main_login_area.classList.add('forgot');
+}
+
+function mobile_log_cross(){
+    var login_register_back= document.getElementById('login_register_back');
+    login_register_back.classList.remove('open')
+}
+function log_click(){
+    var login_register_back= document.getElementById('login_register_back');
+    login_register_back.classList.add('open') 
+}
+
+function after_log_click(){
+    var custom_button= document.getElementById('custom_button');
+    custom_button.classList.toggle('active')
+}
+
+
+
